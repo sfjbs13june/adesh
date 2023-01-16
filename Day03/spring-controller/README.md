2c55c966b9b2f5d315a0d4a181115e81642bd77a
sfjbs <adeshawachat12@gmail.com>
2023-01-16 04:55:45 +0000

    spring-controller

---
+++ Day03/spring-controller/README.md
@@ -0,0 +1,13 @@
+## Spring Controller
+first use https://start.spring.io/ for creating maven project
+
+
+## Copy the folder in day03
+## Delete unnecessary files 
+## Open only spring-controller folder 
+## Build project using cmd : mvn clean install
+## Start project using cmd : mvn spring-boot:run
+
+## Open the port using localhost address in browser and postman
+   # localhost:8080/hello 
+   # localhost:8080/request/hello
---
+++ Day03/spring-controller/src/main/java/com/adesh/app/HelloController.java
@@ -0,0 +1,22 @@
+package com.adesh.app;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class HelloController {
+    @GetMapping("/hello")
+
+    public String SayHello(){
+        return "Hello from app";
+    }
+
+//    Another way to create controller
+
+    @RequestMapping("/request/hello")
+    public String requestHello(){
+        return "Hello from Request Mapping";
+    }
+}
